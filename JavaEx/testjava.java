c4af1c2c4123de68e564b49a969e2ea5b79e4304
SleepingDragon01KarLaNaga <37214051+SleepingDragon01KarLaNaga@users.noreply.github.com>
2018-03-09 19:56:53 +0630

    Add files via upload

---
+++ JavaEx/testjava.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class testjava{
+	public Scanner input=new Scanner(System.in);
+	public static void main(String[] args){
+		String[] marks=new int [20];
+		System.out.print("Enter the marks: ");
+		marks = input.nextLine();
+		StringTokenizer stk = new StringTokenizer();
+		
+		marks = stk.nextToken(" ");
+		
+		
+	}
+}
